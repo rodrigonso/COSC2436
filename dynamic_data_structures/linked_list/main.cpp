a0264f5d770deb87a01f90cc62f053c5d446f236
Rodrigo <rodrigocm08@gmail.com>
2021-09-08 15:12:40 -0500

    created custom linked_list class

---
+++ dynamic_data_structures/linked_list/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "linked_list.cpp"
+
+using namespace std;
+
+int main()
+{
+    LinkedList list;
+
+    list.insertAtBeg(10);
+    list.insertAtBeg(20);
+    list.insertAtBeg(30);
+    list.insertAtBeg(40);
+    list.insertAtBeg(50);
+
+    list.print();
+    Node *testTwo = list.insertAtIdx(100, 0);
+    cout << endl;
+    list.print();
+    cout << endl;
+    Node *test = list.insertAtEnd(200);
+    list.print();
+    cout << endl;
+    list.removeNode(test);
+    list.removeNode(testTwo);
+    list.print();
+
+    return 0;
+}
